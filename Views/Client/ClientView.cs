20c1159811180f903d0516cf5a6acdf1cf7e2fbd
LucasWiest <112181010+LucasWiest@users.noreply.github.com>
2024-07-24 18:59:33 -0300

    mock e teste unitario

--- Views/Client/ClientView.cs
+++ Views/Client/ClientView.cs
@@ -6,14 +6,14 @@ namespace ProjetoTeste.Views.Client
         public required Guid Id { get; set; }
 
         public required string Name { get; set; }
-        public DateOnly Birth { get; set; }
+        public required DateOnly Birth { get; set; }
         public required string Profession { get; set; }
 
-        public decimal GrossSalary { get; set; }
+        public required decimal GrossSalary { get; set; }
 
-        public decimal Discount { get; set; }
+        public required decimal Discount { get; set; }
 
-        public decimal NetSalary { get; set; }
+        public required decimal NetSalary { get; set; }
 
     }
 }
